0c4072194c241ad360bfec2a24e123f0948ab23f
yudhaindra <93323747+yudhaindra@users.noreply.github.com>
2021-11-11 20:04:49 +0700

    Tambah Soal 2

---
+++ 2_B_71210716.py
@@ -0,0 +1,9 @@
+print("Menghitung Luas Permukaan Tabung") 
+
+r = float(input("Masukkan jari-jari alas tabung : "))
+t = float(input("Masukkan tinggi tabung : "))
+phi = 22/7
+
+V = phi*r**2*t
+
+print ("Luas Permukaan Tabung adalah ",V)
